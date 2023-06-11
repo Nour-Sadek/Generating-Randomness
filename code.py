4106067a970d4db65788084257a3e19ac9ccbbae
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-06-11 09:34:20 +0300

    Added Description and Objectives for Stages 1 and 2's docstrings

--- code.py
+++ code.py
@@ -1,4 +1,28 @@
-"""Stage One: Input Processing"""
+"""Stage One: Input Processing
+
+Description
+
+To train the machine to predict the next user input, we need to collect data
+about the user. We will ask them to press 0's and 1's on the keyboard in an
+unpredictable order. These data will be kept in a string of the format
+011100101010… Other characters will not be considered. If a user makes a mistake
+and presses 2 instead of 1 or enters characters separated by commas, spaces or
+any other character — they will be excluded from the input string.
+
+Objectives
+
+1 - Store the value of the minimum length of a string of zeros and ones that the
+user must input.
+2 - Ask user to enter a random string of 0 and 1.
+3 - Read user input and filter out inappropriate symbols from each user input.
+4 - Append the processed string to the general string containing all the data
+from the inputs.
+5 - Keep asking the user for new input strings and appending them to the general
+string until the length of the general string is equal to or exceeds the
+specified minimum string length.
+6 - Output the final data string.
+
+"""
 
 MIN_LEN = 100
 
@@ -52,4 +76,23 @@ while len(final_string) < MIN_LEN:
 print('Final data string:')
 print(final_string)
 
-"""Stage Two: Analyzing User Input"""
+"""Stage Two: Analyzing User Input
+
+Description
+
+We will create a "profile" of the user that will contain information about 
+patterns found in their "random" clicks. To do this, we will count how many 
+times a certain character (0 or 1) follows a specific triad of numbers. Examples 
+of triads: 000 or 011. For example, in the string 00010000, the triad 000 is 
+followed by a 1 and once by 0.
+
+Objectives
+
+1 - Get all possible triads from the final string. For each of them, count the
+number of 0 and 1 that follow them.
+2 - Output the result in the following format: [triad: {count_of_0}, 
+{count_of_1}], for example, [000: 57, 12]. The result of each triad will be
+printed on a new line, ordered in ascending order of their decimal 
+representation.
+
+"""
